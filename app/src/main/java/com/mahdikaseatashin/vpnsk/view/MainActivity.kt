d0c0b7900c6b5755a35388c31d49581bc5f43f49
Mahdi Kase Atashin <mahdikaseatashin@gmail.com>
2021-11-07 12:11:06 +0330

    + data binding + home fragment + navigation component

--- app/src/main/java/com/mahdikaseatashin/vpnsk/view/MainActivity.kt
+++ app/src/main/java/com/mahdikaseatashin/vpnsk/view/MainActivity.kt
@@ -2,12 +2,18 @@ package com.mahdikaseatashin.vpnsk.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.mahdikaseatashin.vpnsk.R
+import com.mahdikaseatashin.vpnsk.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
