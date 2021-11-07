6d65ddc6c4b90c1545eba9f18083410f5f6dc195
Mahdi Kase Atashin <mahdikaseatashin@gmail.com>
2021-11-07 10:09:04 +0330

    + dependency injection basic and gradle

--- app/src/main/java/com/mahdikaseatashin/vpnsk/view/MainActivity.kt
+++ app/src/main/java/com/mahdikaseatashin/vpnsk/view/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.mahdikaseatashin.vpnsk
+package com.mahdikaseatashin.vpnsk.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mahdikaseatashin.vpnsk.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
